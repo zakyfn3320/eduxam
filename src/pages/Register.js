32a556de0ed720a68b55a0bc8db2018eef347cfb
Zaky Fauzan Naufal <zakyfn0303@gmail.com>
2022-06-24 15:32:30 +0700

    eduxam: add register components

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import TextInput from '../components/TextInput'
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className='h-screen flex justify-center items-center'>
+            <div className='bg-white w-5/6 md:w-4/5 lg:w-1/2 rounded-3xl drop-shadow-xl'>
+                <div className='p-10'>
+                    <h1 className='text-2xl font-bold'>Daftar Akun Baru</h1>
+                    <br />
+                    <div className="grid md:gap-5 md:grid-cols-2">
+                        <TextInput type="text">Nama</TextInput>
+                        <TextInput type="email">Email</TextInput>
+                    </div>
+                    <div className="grid md:gap-5 md:grid-cols-2">
+                        <TextInput type="text">Jurusan</TextInput>
+                        <TextInput type="text">Jurusan</TextInput>
+                    </div>
+                    <div className="grid md:gap-5 md:grid-cols-2 mb-5">
+                        <TextInput type="password">Kata Sandi</TextInput>
+                        <TextInput type="password">Konfirmasi Kata Sandi</TextInput>
+                    </div>
+                    <div className="mb-2">
+                        <Link to='/login' className='p-3 bg-red-500 rounded-lg text-white font-bold shrink'><span>Masuk</span></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
