e678dc74179da7f177d2b0755de16f0c063e42ed
Zaky Fauzan Naufal <zakyfn0303@gmail.com>
2022-06-24 15:20:05 +0700

    eduxam: add register button on login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,20 +6,23 @@ const Login = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
             <div className='bg-white w-5/6 md:w-4/5 lg:w-1/2 rounded-3xl drop-shadow-xl'>
-                <div>
-
-                    <div className="grid grid-cols h-full">
-                        <div className='p-10'>
-                            <h1 className='text-2xl font-bold'>Masuk ke akun Anda</h1>
-                            <br />
-                            <TextInput type="email">Email</TextInput>
-                            <TextInput type="password">Password</TextInput>
-                            <br />
-                            <Link to='/' className='p-3 bg-red-500 rounded-lg text-white font-bold'>Masuk</Link>
+                <div className="grid grid-cols">
+                    <div className='p-10 block'>
+                        <h1 className='text-2xl font-bold'>Masuk ke akun Anda</h1>
+                        <br />
+                        <TextInput type="email">Email</TextInput>
+                        <TextInput type="password">Password</TextInput>
+                        <br />
+                        <div className="grid place-content-center">
+                            <div className="flex justify-center mb-2">
+                                <Link to='/' className='p-3 bg-red-500 rounded-lg text-white font-bold shrink'><span>Masuk</span></Link>
+                            </div>
+                            <h3 className=''>Belum memiliki akun? silakan <Link to='/register' className='underline'>daftar</Link></h3>
+                            <div>
+                            </div>
                         </div>
-                        <img className='rounded-r-3xl h-96 w-full object-cover col-span-1 col-end-6' src="https://images.pexels.com/photos/1595391/pexels-photo-1595391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="people studying" />
-
                     </div>
+                    <img className='rounded-r-3xl h-full w-full object-cover col-span-1 col-end-6' src="https://images.pexels.com/photos/1595391/pexels-photo-1595391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="people studying" />
                 </div>
             </div>
         </div>
