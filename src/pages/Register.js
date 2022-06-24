7df92c0c79b9fed3271cf26b697ad13a9e207144
Zaky Fauzan Naufal <zakyfn0303@gmail.com>
2022-06-24 19:37:24 +0700

    eduxam: add auth to register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import TextInput from '../components/TextInput'
+import { AuthenticationContext } from '../hooks/Authentication'
 
 const Register = () => {
+
+    const [auth, setAuth] = useState({ email: '', password: '' })
+
+    const authentication = useContext(AuthenticationContext)
+
+    useEffect(() => {
+        console.log('Authentication', authentication)
+    }, [authentication])
+
+    const doSignIn = () => {
+        authentication.setUserState(auth)
+        console.log('first', auth)
+    }
+
+    const handleValue = (e) => {
+        setAuth(prev => ({
+            ...prev, [e.target.name]: e.target.value
+        }))
+    }
+
     return (
         <div className='py-32 flex justify-center items-center'>
             <div className='bg-white w-5/6 md:w-4/5 lg:w-1/2 rounded-3xl drop-shadow-xl'>
@@ -10,19 +31,19 @@ const Register = () => {
                     <h1 className='text-2xl font-bold'>Daftar Akun Baru</h1>
                     <br />
                     <div className="grid md:gap-5 md:grid-cols-2">
-                        <TextInput type="text">Nama</TextInput>
-                        <TextInput type="email">Email</TextInput>
+                        <TextInput onChange={handleValue} name='name' type="text">Nama</TextInput>
+                        <TextInput onChange={handleValue} name='email' type="email">Email</TextInput>
                     </div>
                     <div className="grid md:gap-5 md:grid-cols-2">
-                        <TextInput type="text">Jurusan</TextInput>
-                        <TextInput type="text">Jurusan</TextInput>
+                        <TextInput onChange={handleValue} name='major' type="text">Jurusan</TextInput>
+                        <TextInput onChange={handleValue} name='class' type="text">Kelas</TextInput>
                     </div>
                     <div className="grid md:gap-5 md:grid-cols-2 mb-5">
-                        <TextInput type="password">Kata Sandi</TextInput>
-                        <TextInput type="password">Konfirmasi Kata Sandi</TextInput>
+                        <TextInput onChange={handleValue} name='password' type="password">Kata Sandi</TextInput>
+                        <TextInput onChange={handleValue} name='passwordConfirm' type="password">Konfirmasi Kata Sandi</TextInput>
                     </div>
                     <div className="mb-2">
-                        <Link to='/login' className='p-3 bg-red-500 rounded-lg text-white font-bold shrink'><span>Masuk</span></Link>
+                        <button onClick={doSignIn} className='p-3 bg-red-500 rounded-lg text-white font-bold shrink'><span>Masuk</span></button>
                     </div>
                 </div>
             </div>
